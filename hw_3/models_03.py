47e9a18ba36793a101ee21afdb3b6d382e3c2a77
Kokin Sergei <kokinsergei@gmail.com>
2023-05-29 17:46:44 +0300

    Added hw_3

---
+++ hw_3/models_03.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
